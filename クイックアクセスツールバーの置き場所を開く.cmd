0d3454ca0063610dfe9c9078e73e59034be251d6
ボクはお金で動くよ <70741260+black-triple-star@users.noreply.github.com>
2020-10-08 10:42:08 +0900

    1st

---
+++ クイックアクセスツールバーの置き場所を開く.cmd
@@ -0,0 +1,11 @@
+chcp 65001
+chcp 932
+
+rem MS office アドインディレクトリを開く
+
+rem cd /d "%USERPROFILE%\AppData\Roaming\Microsoft\AddIns"
+rem start .
+
+rem qatファイルの場所
+cd /d "%USERPROFILE%\AppData\Local\Microsoft\Office"
+start .
